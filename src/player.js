e9eafa65495bdfdce030c45097e1c3f5a26ba365
Hadis <hadiswebpro@gmail.com>
2026-09-15 23:13:01 +0330

    Prepare players for manual ship placement

--- src/player.js
+++ src/player.js
@@ -2,108 +2,80 @@ import Gameboard from "./gameboard";
 import Ship from "./ship";
 
 export default class Player {
-  constructor(name, color) {
+  constructor(name, color, autoPlace = true) {
     this.gameboard = new Gameboard();
     this.name = name;
     this.color = color;
     this.attacksMade = new Set();
     this.targetQueue = [];
+    this.shipLengths = [5, 4, 3, 3, 2];
 
-    this.placeShips();
+    if (autoPlace) this.placeShips();
   }
 
   placeShips() {
-    const shipLengths = [5, 4, 3, 3, 2];
-
-    shipLengths.forEach((length) => {
+    this.shipLengths.forEach((length) => {
       let placed = false;
 
       while (!placed) {
-        const ship = new Ship(length);
-        const coordinates = this.generateCoordinates(length);
-
         try {
-          this.gameboard.placeShip(ship, coordinates);
+          this.gameboard.placeShip(
+            new Ship(length),
+            this.generateCoordinates(length)
+          );
           placed = true;
-        } catch (error) {
-          placed = false;
-        }
+        } catch (error) {}
       }
     });
+  }
+
+  placeShip(length, coordinates) {
+    const ship = new Ship(length);
+    this.gameboard.placeShip(ship, coordinates);
   }
 
   generateCoordinates(length) {
     const horizontal = Math.random() > 0.5;
     const row = Math.floor(Math.random() * 10);
     const col = Math.floor(Math.random() * 10);
-
     const coordinates = [];
 
     for (let i = 0; i < length; i++) {
-      const newRow = horizontal ? row : row + i;
-      const newCol = horizontal ? col + i : col;
+      const r = horizontal ? row : row + i;
+      const c = horizontal ? col + i : col;
 
-      if (newRow > 9 || newCol > 9) {
-        return this.generateCoordinates(length);
-      }
-
-      coordinates.push([newRow, newCol]);
+      if (r > 9 || c > 9) return this.generateCoordinates(length);
+      coordinates.push([r, c]);
     }
 
     return coordinates;
   }
 
   attack(enemyBoard, coordinate) {
-    const key = coordinate.toString();
-    this.attacksMade.add(key);
-
+    this.attacksMade.add(coordinate.toString());
     return enemyBoard.receiveAttack(coordinate);
   }
 
   computerAttack(enemyBoard) {
     let coordinate;
-    let key;
 
     do {
-      if (this.targetQueue.length > 0) {
-        coordinate = this.targetQueue.shift();
-      } else {
-        coordinate = [
-          Math.floor(Math.random() * 10),
-          Math.floor(Math.random() * 10),
-        ];
-      }
+      coordinate = this.targetQueue.length
+        ? this.targetQueue.shift()
+        : [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    } while (this.attacksMade.has(coordinate.toString()));
 
-      key = coordinate.toString();
-    } while (this.attacksMade.has(key));
-
-    this.attacksMade.add(key);
-
+    this.attacksMade.add(coordinate.toString());
     const result = enemyBoard.receiveAttack(coordinate);
 
-    if (result === "hit") {
-      this.addTargetsAround(coordinate);
-    }
-
+    if (result === "hit") this.addTargetsAround(coordinate);
     return result;
   }
 
   addTargetsAround([row, col]) {
-    const targets = [
-      [row - 1, col],
-      [row + 1, col],
-      [row, col - 1],
-      [row, col + 1],
-    ];
-
-    targets.forEach(([r, c]) => {
-      if (r >= 0 && r < 10 && c >= 0 && c < 10) {
-        const key = [r, c].toString();
-
-        if (!this.attacksMade.has(key)) {
-          this.targetQueue.push([r, c]);
-        }
-      }
+    [[row-1,col],[row+1,col],[row,col-1],[row,col+1]].forEach(([r,c])=>{
+      const key=[r,c].toString();
+      if(r>=0&&r<10&&c>=0&&c<10&&!this.attacksMade.has(key)) this.targetQueue.push([r,c]);
     });
   }
 }
