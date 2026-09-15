50e7ce39d16e462cbf82de0806d4d71c67eee4a3
Hadis <hadiswebpro@gmail.com>
2026-09-16 01:24:08 +0330

    Separate turn handling from computer attacks

--- src/game.js
+++ src/game.js
@@ -6,8 +6,7 @@ export default class Game {
 
     this.player1 = new Player("Player", "blue", false);
     this.player2 = new Player(
-      mode === "computer" ? "Computer" : "Player 2",
-      "blue"
+      mode === "computer" ? "Computer" : "Player 2"
     );
 
     this.currentPlayer = this.player1;
@@ -30,28 +29,53 @@ export default class Game {
     return player === this.player1 ? this.player2 : this.player1;
   }
 
-  playTurn(coordinate) {
+  attack(coordinate) {
     const opponent = this.getOpponent(this.currentPlayer);
-    const result = this.currentPlayer.attack(opponent.gameboard, coordinate);
+    const result = this.currentPlayer.attack(
+      opponent.gameboard,
+      coordinate
+    );
 
     if (result !== "already") {
       this.switchTurn();
+    }
 
-      if (
-        this.mode === "computer" &&
-        this.currentPlayer === this.player2 &&
-        !this.isGameOver()
-      ) {
-        this.player2.computerAttack(this.player1.gameboard);
-        this.switchTurn();
-      }
+    return result;
+  }
+
+  computerTurn() {
+    if (
+      this.mode !== "computer" ||
+      this.currentPlayer !== this.player2 ||
+      this.isGameOver()
+    ) {
+      return null;
+    }
+
+    const result = this.player2.computerAttack(
+      this.player1.gameboard
+    );
+
+    this.switchTurn();
+
+    return result;
+  }
+
+  playTurn(coordinate) {
+    const result = this.attack(coordinate);
+
+    if (result !== "already") {
+      this.computerTurn();
     }
 
     return result;
   }
 
   isGameOver() {
-    return this.player1.gameboard.allShipsSunk() || this.player2.gameboard.allShipsSunk();
+    return (
+      this.player1.gameboard.allShipsSunk() ||
+      this.player2.gameboard.allShipsSunk()
+    );
   }
 
   getWinner() {
