233e36e5b8da4a8b637282a9217f7fd365bc6403
Hadis <hadiswebpro@gmail.com>
2026-09-15 17:06:16 +0330

    Keep player and enemy board themes consistent

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
 import Game from "./game";
 
 import {
-    createBoard,
-    addAttackListener
+  createBoard,
+  addAttackListener
 } from "./dom";
 
 
 const game = new Game();
 
+const PLAYER_COLOR = "green";
+const ENEMY_COLOR = "red";
 
-const playerBoard =
-document.querySelector("#player-board");
-
-
-const enemyBoard =
-document.querySelector("#enemy-board");
-
-
+const playerBoard = document.querySelector("#player-board");
+const enemyBoard = document.querySelector("#enemy-board");
 
 createBoard(
-    playerBoard,
-    "green"
+  playerBoard,
+  PLAYER_COLOR
 );
-
 
 createBoard(
-    enemyBoard,
-    "red"
+  enemyBoard,
+  ENEMY_COLOR
 );
-
 
 addAttackListener(
-    enemyBoard,
-    game
+  enemyBoard,
+  game
 );
