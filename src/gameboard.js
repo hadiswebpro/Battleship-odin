1391923fb98c3f57710e8fdbdaae2594e6a2cd38
Hadis <hadiswebpro@gmail.com>
2026-09-16 14:31:20 +0330

    Improve gameboard ship tracking and attack system

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,222 +1,87 @@
 export default class Gameboard {
-
   constructor() {
-
     this.ships = [];
-
     this.board = new Map();
-
     this.missedAttacks = [];
-
     this.attackedCoordinates = new Set();
-
   }
-
-
 
   placeShip(ship, coordinates) {
-
-
     if (coordinates.length !== ship.length) {
-
-      throw new Error(
-        "Invalid ship placement"
-      );
-
+      throw new Error("Invalid ship placement");
     }
 
+    const keys = coordinates.map((position) => position.toString());
 
+    if (keys.some((key) => this.board.has(key))) {
+      throw new Error("Position already occupied");
+    }
 
-    coordinates.forEach(position => {
-
-
-      const key =
-        position.toString();
-
-
-
-      if(this.board.has(key)){
-
-        throw new Error(
-          "Position already occupied"
-        );
-
-      }
-
-
-
-      this.board.set(
-        key,
-        ship
-      );
-
-
+    keys.forEach((key) => {
+      this.board.set(key, ship);
     });
 
-
-
     this.ships.push(ship);
-
   }
 
-
-
-
-
-  getShipAt(coordinate){
-
-
-    return this.board.get(
-      coordinate.toString()
-    );
-
-
+  getShipAt(coordinate) {
+    return this.board.get(coordinate.toString());
   }
 
+  receiveAttack(coordinate) {
+    const key = coordinate.toString();
 
-
-
-
-
-  receiveAttack(coordinate){
-
-
-    const key =
-      coordinate.toString();
-
-
-
-    // جلوگیری از تیر دوباره
-
-    if(
-      this.attackedCoordinates.has(key)
-    ){
-
+    if (this.attackedCoordinates.has(key)) {
       return "already";
-
     }
-
-
-
 
     this.attackedCoordinates.add(key);
 
+    const ship = this.board.get(key);
 
-
-
-    const ship =
-      this.board.get(key);
-
-
-
-
-    if(ship){
-
-
+    if (ship) {
       ship.hit();
-
-
       return "hit";
-
-
     }
 
-
-
-
-    this.missedAttacks.push(
-      coordinate
-    );
-
-
-
+    this.missedAttacks.push(coordinate);
     return "miss";
-
-
   }
 
-
-
-
-
-
-
-  hasBeenAttacked(coordinate){
-
-
-    return this.attackedCoordinates.has(
-      coordinate.toString()
-    );
-
-
+  hasBeenAttacked(coordinate) {
+    return this.attackedCoordinates.has(coordinate.toString());
   }
 
+  getAttackResult(coordinate) {
+    const key = coordinate.toString();
 
-
-
-
-
-
-  getAttackResult(coordinate){
-
-
-    const key =
-      coordinate.toString();
-
-
-
-    if(
-      !this.attackedCoordinates.has(key)
-    ){
-
+    if (!this.attackedCoordinates.has(key)) {
       return null;
-
     }
 
-
-
-    return this.board.has(key)
-      ? "hit"
-      : "miss";
-
-
+    return this.board.has(key) ? "hit" : "miss";
   }
 
-
-
-
-
-
-
-  allShipsSunk(){
-
-
-    return this.ships.length > 0 &&
-      this.ships.every(
-        ship => ship.isSunk()
-      );
-
-
+  getShipStatus() {
+    return this.ships.map((ship) => ({
+      name: ship.name,
+      length: ship.length,
+      hits: ship.hits,
+      sunk: ship.isSunk(),
+    }));
   }
 
+  allShipsSunk() {
+    return (
+      this.ships.length === 5 &&
+      this.ships.every((ship) => ship.isSunk())
+    );
+  }
 
-
-
-
-
-
-  clear(){
-
-
+  clear() {
     this.ships = [];
-
     this.board.clear();
-
     this.missedAttacks = [];
-
     this.attackedCoordinates.clear();
-
-
   }
-
 }
